ba53a03403a6adec70d5d904511ddd7b3d23914b
Israel Lund <israelmlund@gmail.com>
2021-09-24 12:37:33 -0400

    dflkjdlfj

---
+++ bash.js
@@ -0,0 +1,6 @@
+process.stdout.write("prompt > ");
+process.stdin.on("data", (data) => {
+	const cmd = data.toString().trim();
+	process.stdout.write("you typed: " + cmd);
+	process.stdout.write("\nprompt > ");
+});
